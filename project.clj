288873aeb174f41aed6974e512918cfd47cdef4d
Ben Mabey <ben@benmabey.com>
2015-09-24 10:35:26 -0600

    move clojure dep into dev ns

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/redbrainlabs/system-graph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.stuartsierra/component "0.2.1"]
+  :dependencies [[com.stuartsierra/component "0.2.1"]
                  [prismatic/plumbing "0.4.3"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.3-RC2"]]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [org.clojure/clojure "1.5.1"]
                                   [midje "1.6-beta1"]]
                    :source-paths ["dev"]}}
   :aliases {"dumbrepl" ["trampoline" "run" "-m" "clojure.main/main"]})
