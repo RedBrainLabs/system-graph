f56e32d95bdf0f81e4627b3538e4c24cc09c130d
Ben Mabey <ben@benmabey.com>
2015-09-24 10:38:34 -0600

    bumpts component to 0.2.3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/redbrainlabs/system-graph"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.stuartsierra/component "0.2.1"]
+  :dependencies [[com.stuartsierra/component "0.2.3"]
                  [prismatic/plumbing "0.5.0"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.3-RC2"]]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
